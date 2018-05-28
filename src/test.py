bc6381f11e1c7862c3f91290f0a523124dddf974
hylsceptic <thesceptic@126.com>
2018-05-28 12:24:21 +0800

    minor changes

--- src/test.py
+++ src/test.py
@@ -1,5 +1,5 @@
 from random import random
-<<<<<<< HEAD
+
 
 # try:
 # 	a=1/0
@@ -8,22 +8,3 @@ from random import random
 # 	raise Exception('yichang')
 # print(2)
 print(random())
-=======
-import math
-def sign(num):
-    if num >=0:
-        return 1
-    else:
-        return -1
-def getAngleByVector(v):
-    v=[v[0], v[1]]
-    angle0 = 0
-    if v[0] < 0:
-    	v = [-v[0], -v[1]]
-    	angle0 += math.pi
-    angle = math.asin(v[1]/math.sqrt(v[0]**2 + v[1]**2))
-    return angle + angle0
-
-
-print(getAngleByVector([-1, 1])/math.pi)
->>>>>>> hyl_temp
